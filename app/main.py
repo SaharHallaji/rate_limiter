9e10ed7dc891af9aaca88491c70896697516c9f7
sahar <saharhallaji.dev@gmail.com>
2024-08-06 15:03:05 +0330

    project init.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
 import redis
 import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 app = FastAPI()
 
 # Connect to Redis
-redis_host = os.getenv("REDIS_HOST", "localhost")
-r = redis.Redis(host=redis_host, port=6379, decode_responses=True)
+redis_port = os.getenv("REDIS_PORT")
+redis_host = os.getenv("REDIS_HOST")
+redis_db = os.getenv("REDIS_DB")
+r = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
+
+# Test Redis connection
+try:
+    r.ping()
+    print("Redis connected.")
+except redis.ConnectionError:
+    raise Exception("Redis connection failed.")
 
 
 @app.get("/")
-async def read_root():
-    await r.set('foo', 'bar')
+def root():
+    r.set("foo", "bar")
     return {"message": "Hello World", "redis_value": r.get('foo')}
